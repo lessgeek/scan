dea5a1f0912f0b9271684675f55efeb1f1e8e170
lessgeek <156588103@qq.com>
2025-08-11 00:58:37 +0800

    Create test.vue

---
+++ fes/test.vue
@@ -0,0 +1,31 @@
+// This shows examples related with Vue.js.
+
+<script>
+export default {
+  methods: {
+    update() {}
+  },
+  mounted() {
+    window.addEventListener('hashchange', this.update, false);
+  },
+  beforeDestroy() {
+    window.removeEventListener('hashChange', this.update, false);
+  }
+}
+</script>
+
+<template>
+  <div :class="fooClass" :v-if="isFoo">
+    <div v-if="shouldHi">Hi</div>
+    <div v-else="shouldBye">Bye</div>
+    <ul>
+      <li v-if="users" v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+    <comp v-for="item in items">
+      {{ item.name }}
+    </comp>
+    <template v-show="flag">
+      Only show me if flag is true!
+    </template>
+  </div>
+</template>
